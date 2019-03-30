7067d3af39013dd27f293a78181ddb9a06462896
DavidH <toorampage@toorampage.com>
2019-03-30 10:45:17 -0300

    Added a cards router with a cards.pug file that has a simple search box.

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var MongoClient = require('mongodb').MongoClient;
+var database = require('./config/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var cardsRouter = require('./routes/cards');
 
 var app = express();
 
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +42,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+// -- Set up which database we are connecting to according to command line
+//    arguments.
+var dbhost = database.url;
+console.log(" -- setting dbhost as "+dbhost);
 module.exports = app;
